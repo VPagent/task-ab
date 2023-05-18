dda4df789290bf8be1fbb1ea967966d49c3b2779
VPagent <vpagent13@gmail.com>
2023-05-18 21:37:20 +0300

    fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import Container from "../../components/Container/Container";
 import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
-import { ItemsProvider } from "../../components/context/itemsContext";
+import { ItemsProvider } from "../../components/context/ItemsContext";
 
 import styles from "./HomePage.module.scss";
 import { FC, useState } from "react";
